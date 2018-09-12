39ed1756d01b0905f8020c3b9813ac2b88940268
John Romby <au518762@uni.au.dk>
2018-09-12 17:17:02 +0200

    Added a test project to try out simple Data Binding

---
+++ Test/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Test
+{
+    public partial class MainWindow : INotifyPropertyChanged
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        private void Reset_OnClick(object sender, RoutedEventArgs e)
+        {
+            BoundNumber = 0;
+        }
+
+        private int _boundNumber;
+        public int BoundNumber
+        {
+            /*get { return _boundNumber; }*/    // Not using Lambda Expression
+            get => _boundNumber;                // Using Lambda Expression
+
+            set
+            {
+                if (_boundNumber != value)
+                {
+                    _boundNumber = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
